249fe7e8bfb8d68c205506632d122bee44e51506
이해경 <ondal@ihaegyeong-ui-MacBookAir.local>
2023-12-01 00:13:30 +0900

    test

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var port = 8080;
 http.createServer( (req, res) => {
   console.log('Requested: ' + req.url);
   res.writeHead(200, { 'Content-Type':'text/html; charset=utf-8' });
-  res.write('<h1>docker 예제1</h1>');
+  res.write('<h1>docker 예제</h1>');
   res.write('<h2>Hello node!</h2>');
   res.end();
 }).listen(port, () => {
